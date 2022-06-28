adab383698bd20817ee5bc766c976ee4de390996
du <996158465@qq.com>
2022-06-28 18:33:31 +0800

    完成

---
+++ index1.js
@@ -0,0 +1,43 @@
+const content = document.getElementById('content')
+const date = document.getElementById('date')
+const time = document.getElementById('time')
+const addedBtn = document.getElementById('addedBtn')
+const deletedBtn = document.getElementById('deletedBtn')
+const list = document.getElementById('list')
+
+const listContent = []
+
+function render() {
+    // 渲染页面的list
+    let htmlStr = ''
+
+    listContent.forEach(function (item) {
+        htmlStr = htmlStr + `
+        <div class="item">
+            <div>
+                <p>内容：${item.content}</p>
+                <p>时间：${item.date} ${item.time}</p>
+            </div>
+        </div>
+        `
+    })
+
+    list.innerHTML = htmlStr
+}
+
+addedBtn.addEventListener('click', function() {
+
+    listContent.unshift({
+        content: content.value,
+        date: date.value,
+        time: time.value
+    })
+
+    render()
+})
+
+deletedBtn.addEventListener('click', function () {
+    listContent.shift()
+
+    render()
+})
